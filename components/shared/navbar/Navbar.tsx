ca1adc798b3b0d6fe1a58bec168ec3141958b933
ast_vnovodid <v.novodid@astoundcommerce.com>
2024-08-03 19:55:20 +0300

    feat: add global search

--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image';
 import { SignedIn, UserButton } from '@clerk/nextjs';
 import Theme from '@/components/shared/navbar/Theme';
 import MobileNav from '@/components/shared/navbar/MobileNav';
+import GlobalSearch from '@/components/shared/search/GlobalSearch';
 
 const Navbar = () => {
     return (
@@ -26,7 +27,7 @@ const Navbar = () => {
                     Dev <span className='text-primary-500'>Overflow</span>
                 </p>
             </Link>
-            GlobalSearch
+            <GlobalSearch />
             <div className='flex-between gap-5'>
                 <Theme />
                 <SignedIn>
