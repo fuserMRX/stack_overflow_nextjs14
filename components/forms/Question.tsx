b5b47ffd9581db53e1a8ecb2fe2671081eafc2bd
ast_vnovodid <v.novodid@astoundcommerce.com>
2024-08-28 11:58:41 +0300

    feat: add multiple tags input

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -1,11 +1,13 @@
 'use client';
 
+import React, { useRef } from 'react';
 import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { Button } from '@/components/ui/button';
-import { useRef } from 'react';
 import { Editor } from '@tinymce/tinymce-react';
+import { Badge } from '@/components/ui/badge';
+import Image from 'next/image';
 
 import {
     Form,
@@ -38,6 +40,40 @@ const Question = () => {
         // ✅ This will be type-safe and validated.
         console.log(values);
     }
+
+    const handleInputKeyDown = (
+        e: React.KeyboardEvent<HTMLInputElement>,
+        field: any
+    ) => {
+        if (e.key === 'Enter' && field.name === 'tags') {
+            e.preventDefault();
+            const tagInput = e.target as HTMLInputElement;
+            const tagValue = tagInput.value.trim();
+
+            if (tagValue.length) {
+                if (tagValue.length > 15) {
+                    return form.setError('tags', {
+                        type: 'required',
+                        message: 'Tag must be less than 15 characters',
+                    });
+                }
+
+                if (!field.value.includes(tagValue as never)) {
+                    form.setValue('tags', [...field.value, tagValue]);
+                    tagInput.value = '';
+                    form.clearErrors('tags');
+                }
+            } else {
+                form.trigger();
+            }
+        }
+    };
+
+    const handleTagRemove = (tag: string, field: any) => {
+        const newTags = field.value.filter((t: string) => t !== tag);
+
+        form.setValue('tags', newTags);
+    };
 
     return (
         <Form {...form}>
@@ -75,7 +111,7 @@ const Question = () => {
                     render={({ field }) => (
                         <FormItem className='flex w-full flex-col gap-3'>
                             <FormLabel className='paragraph-semibold text-dark400_light800'>
-                                Detailed explanation of your problem
+                                Detailed explanation of your problem{' '}
                                 <span className='text-primary-500'>*</span>
                             </FormLabel>
                             <FormControl className='mt-3.5'>
@@ -132,17 +168,49 @@ const Question = () => {
                     render={({ field }) => (
                         <FormItem className='flex w-full flex-col'>
                             <FormLabel className='paragraph-semibold text-dark400_light800'>
-                                Tags
-                                <span className='text-primary-500'>*</span>
+                                Tags <span className='text-primary-500'>*</span>
                             </FormLabel>
                             <FormControl className='mt-3.5'>
-                                <Input
-                                    placeholder='Add tags...'
-                                    className='paragraph-regular no-focus
+                                <>
+                                    <Input
+                                        placeholder='Add tags...'
+                                        className='paragraph-regular no-focus
                     placeholder background-light800_dark400 light-border-2
                     text-dark300_light700 min-h-[46px] border'
-                                    {...field}
-                                />
+                                        onKeyDown={(e) =>
+                                            handleInputKeyDown(e, field)
+                                        }
+                                    />
+                                    {!!field.value.length && (
+                                        <div className='flex-start mt-2.5 gap-2.5'>
+                                            {field.value.map((tag) => (
+                                                <Badge
+                                                    key={tag}
+                                                    className='subtle-medium
+                                                background-light800_dark300 text-light400_light500
+                                                flex items-center justify-center gap-2 rounded-md border-none
+                                                px-4 py-2 capitalize'
+                                                    onClick={() =>
+                                                        handleTagRemove(
+                                                            tag,
+                                                            field
+                                                        )
+                                                    }
+                                                >
+                                                    {tag}
+                                                    <Image
+                                                        src='/assets/icons/close.svg'
+                                                        alt='close'
+                                                        width={12}
+                                                        height={12}
+                                                        className='cursor-pointer
+                                                object-contain invert-0 dark:invert'
+                                                    />
+                                                </Badge>
+                                            ))}
+                                        </div>
+                                    )}
+                                </>
                             </FormControl>
                             <FormDescription className='body-regular mt-2.5 text-light-500'>
                                 Add up to 3 tags to describe what your question
