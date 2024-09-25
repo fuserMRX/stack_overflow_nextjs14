86cf32bf9ba63448d9599c4810979df630acebc4
ast_vnovodid <v.novodid@astoundcommerce.com>
2024-09-25 15:51:54 +0300

    feat: implement create answer action

---
+++ lib/actions/asnwer.action.ts
@@ -0,0 +1,33 @@
+'use server';
+
+import { connectToDatabase } from '@/lib/mongoose';
+import { CreateAnswerParams } from '@/lib/actions/shared.types';
+import Answer from '@/database/answer.model';
+import Question from '@/database/question.model';
+import { revalidatePath } from 'next/cache';
+
+export async function createAnswer(params: CreateAnswerParams) {
+    connectToDatabase();
+
+    const { content, author, question, path } = params;
+
+    const newAnswer = new Answer({
+        content,
+        author,
+        question,
+    });
+
+    // Add the answer to the question's answer array
+    await Question.findByIdAndUpdate(question, {
+        $push: { answers: newAnswer._id },
+    });
+
+    // TODO: add interaction to the user
+    revalidatePath(path);
+    try {
+        // create answer
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
