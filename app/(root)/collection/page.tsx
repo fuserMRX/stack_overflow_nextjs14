cd9433f1554c8ad0f28814117f13d94bebff47e1
ast_vnovodid <v.novodid@astoundcommerce.com>
2024-10-13 18:14:53 +0300

    feat: integrate filters on the collection page

--- app/(root)/collection/page.tsx
+++ app/(root)/collection/page.tsx
@@ -19,6 +19,7 @@ const Collection = async ({ searchParams }: SearchParamsProps) => {
     const { questions } = await getSavedQuestions({
         clerkId,
         searchQuery: searchParams.q,
+        filter: searchParams.filter,
     });
 
     return (
